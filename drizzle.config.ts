1c082c992b2b548b48d2618151a71d397fbb43a0
Rafael Alfaro <r.alfaro.ramirez@accenture.com>
2025-09-18 17:06:02 -0600

    Working Drizzle Migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,13 @@
 import { defineConfig } from "drizzle-kit";
+
 export default defineConfig({
   dialect: "postgresql",
   schema: "./lib/models",
+  out: "./drizzle",  // folder for migrations
+  dbCredentials: {
+    url: process.env.POSTGRES_DB_URI!,
+  },
+  migrations: {
+    schema: 'public'
+  }
 });
