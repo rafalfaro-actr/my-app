a40c168e8e5099913db22e3158d1d2872f398625
Rafael Alfaro <r.alfaro.ramirez@accenture.com>
2025-09-18 17:16:59 -0600

    remove unnecessary setting

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   dialect: "postgresql",
   schema: "./lib/models",
-  out: "./drizzle",  // folder for migrations
   dbCredentials: {
     url: process.env.POSTGRES_DB_URI!,
   },
