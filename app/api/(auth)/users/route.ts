386bd86cb66d75e8b4986791169dbfd74bf15cb5
Rafael Alfaro <r.alfaro.ramirez@accenture.com>
2025-09-18 18:04:38 -0600

    Adds update PATCH user method

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,5 +1,6 @@
 import { connect } from "@/lib/db"
 import { users } from "@/lib/models/user"
+import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
@@ -21,7 +22,26 @@ export const POST = async (request: Request) => {
         
         await db.insert(users).values(body)
 
-        return new NextResponse("Inserted", {status: 201})
+        return new NextResponse("Inserted: " + JSON.stringify(body), {status: 201})
+    } catch (e){
+        console.error(e)
+        return new NextResponse("Error inserting users", {status: 500})
+    }
+}
+
+export const PATCH = async (request: Request) => {
+    try {
+        const body = await request.json()
+        const {id, name} = body
+
+        const db = await connect();
+        // TODO: Validations
+        const updatedUser = await db.update(users)
+            .set({ name: name })
+            .where(eq(users.id, id))
+            .returning();
+
+        return new NextResponse("Updated: " + JSON.stringify(updatedUser), {status: 200})
     } catch (e){
         console.error(e)
         return new NextResponse("Error inserting users", {status: 500})
