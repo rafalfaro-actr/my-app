37e2cf93bafa9a140a6e69702e38835d44c58ea5
Rafael Alfaro <r.alfaro.ramirez@accenture.com>
2025-09-18 23:26:06 -0600

    ADDS UUID Validity comment

--- lib/models/user.ts
+++ lib/models/user.ts
@@ -1,6 +1,6 @@
 import { uuid, text, pgTable } from "drizzle-orm/pg-core";
 
 export const users = pgTable('users', {
-  id: uuid('id').primaryKey(),
+  id: uuid('id').primaryKey(), // MUST BE A VALID UUI OR IT FAILS TO INSERT, USE A VALID GENERATOR ALGORITHM
   name: text('name').notNull()
 });
