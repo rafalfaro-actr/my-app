5f95bf1735d83054ec2be6a1b91465f08568d2e9
Rafael Alfaro <r.alfaro.ramirez@accenture.com>
2025-09-19 11:57:35 -0600

    Fix missing schema definition on db connection

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,12 @@
-import * as schema1 from '@/lib/models/user';
-import * as schema2 from '@/lib/models/membership';
-import * as schema3 from '@/lib/models/role';
+import * as user from '@/lib/models/user';
+import * as membership from '@/lib/models/membership';
+import * as role from '@/lib/models/role';
 import { drizzle } from 'drizzle-orm/node-postgres';
 
 const POSTGRES_DB_URI = process.env.POSTGRES_DB_URI!;
 
 export const connect = async () => {
-    const db = drizzle(POSTGRES_DB_URI, { schema: { ...schema1, ...schema2, ...schema3 } })
+    const db = drizzle(POSTGRES_DB_URI, { schema: { ...user, ...membership, ...role } })
 
     try {
         await db.$client.connect()
